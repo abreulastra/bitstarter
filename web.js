1130baeada76dbdf14d0272c6f3faace61ff0ff3
Raul Abreu Lastra <abreulastra@gmail.com>
2014-03-12 18:05:34 +0000

    prueba version 4, usando string y buff

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express'), fs=require('fs'), buffer=require('buffer');
 var app = express();
-var buffer=fs.readFileSync("index.html");
 App.use(express.logger());
 
 
 app.get('/', function(request, response) {
-  response.send("hipity pop");
+    var buffer=fs.readFileSync("index.html");
+
+    response.send("hipity pop");
 });
 
 var port = process.env.PORT || 5000;
