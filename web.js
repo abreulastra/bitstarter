dee52560fc7fa047cfaa48fdd06f6dc7937364f5
Raul Abreu Lastra <abreulastra@gmail.com>
2014-03-13 10:40:14 +0000

    This version was tested in the in the emacs node reple, does not use buffers

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express'), fs=require('fs'), buffer=require('buffer');
 var app = express();
-var b=fs.readFileSync('index.html');
-App.use(express.logger());
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(b.toString('utf8'));
+    response.send(fs.readSyncFile('index.html',"utf8"));
 });
 
 var port = process.env.PORT || 5000;
