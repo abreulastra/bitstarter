c62592c1f80affcea1bb0d242f45b0b7f8b2b912
Raul Abreu Lastra <abreulastra@gmail.com>
2014-03-13 07:56:22 +0000

    prueba version 8, usanto read y buf

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express'), fs=require('fs'), buffer=require('buffer');
 var app = express();
+var b=fs.readFileSync('index.html');
 App.use(express.logger());
 
 app.get('/', function(request, response) {
-    var b=fs.readFileSync('index.html');
     response.send(b.toString('utf8'));
 });
 
